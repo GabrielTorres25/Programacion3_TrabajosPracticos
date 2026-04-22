5058f799e5a8b344c11a999afb07d01d531dbc84
GabrielTorres25 <gabicjs87@gmail.com>
2026-04-21 21:02:59 -0300

    Trabajo Integrador 3
    Se creo un archivo js para darle funcionalidad al sitio y css para darles estilos

---
+++ Html/js/data.js
@@ -0,0 +1,62 @@
+// Array de categorias
+export const categorias = ["Hamburguesas", "Pizzas", "Postres", "Bebidas"];
+
+//Array de productos
+export const productos = [
+{
+id: 1,
+nombre: "Hamburguesa Triple",
+descripcion: "Triple carne, cheddar y bacon",
+precio: 25000,
+imagen: "img/smash-burger.jpg",
+categoria: "Hamburguesas"
+},
+{
+id: 2,
+nombre: "Pizza Muzzarella",
+descripcion: "Salsa casera y orégano",
+precio: 18000,
+imagen: "img/pizza.avif",
+categoria: "Pizzas"
+},
+{
+id: 3,
+nombre: "Selva negra",
+descripcion: "Torta de chocolate",
+precio: 12000,
+imagen: "img/torta.jpg",
+categoria: "Postres"
+},
+{
+id: 4,
+nombre: "Coca-Cola",
+descripcion: "Refresco de cola",
+precio: 3000,
+imagen: "img/coquita.jpg",
+categoria: "Bebidas"
+},
+{
+id: 5,
+nombre: "Hamburguesa Doble",
+descripcion: "Doble carne, queso y lechuga",
+precio: 20000,
+imagen: "img/smash-burger.jpg",
+categoria: "Hamburguesas"
+},
+{
+id: 6,
+nombre: "Pizza Pepperoni",
+descripcion: "Salsa casera y pepperoni",
+precio: 22000,
+imagen: "img/pizza.avif",
+categoria: "Pizzas"
+},
+{
+id: 7,
+nombre: "Helado de Chocolate",
+descripcion: "Helado de chocolate con trozos de galleta",
+precio: 8000,
+imagen: "img/helado_de_chocolate.jpg",
+categoria: "Postres"
+}
+]
---
+++ Html/js/main.js
@@ -0,0 +1,50 @@
+import { categorias } from "./data.js"
+
+function cargarCategorias(){
+    const lista = document.getElementById("lista-categorias");
+    
+    categorias.forEach(categoria => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+
+        a.textContent = categoria;
+        a.href = "#";
+
+        li.appendChild(a);
+        lista.appendChild(li);
+    });
+}
+
+cargarCategorias();
+
+import { productos } from "./data.js";
+
+function cargarProductos() {
+    const contenedor = document.getElementById("productos");
+
+    productos.forEach(producto => {
+        
+        const article = document.createElement("article");
+        article.classList.add("producto");
+
+        // estructura interna del producto
+        article.innerHTML = `
+            <img src="${producto.imagen}" alt="${producto.nombre}">
+            <h4>${producto.nombre}</h4>
+            <p>${producto.descripcion}</p>
+            <p><strong>$${producto.precio}</strong></p>
+            <button>Agregar</button>
+        `;
+
+        // evento boton
+        const boton = article.querySelector("button");
+        boton.addEventListener("click", () => {
+            alert("Agregaste: " + producto.nombre);
+        });
+
+        // insertar producto en el DOM
+        contenedor.appendChild(article);
+    });
+}
+
+cargarProductos();
